f46c8ef1668ad5d482f6474d8e8e7c7865e0e017
saki <saki@ginuuk.net>
2015-06-23 11:16:26 +0900

    update_list() について日付入力値を変更

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -34,8 +34,10 @@ class ApiController extends AppController
 		// http://ajocc.net/api/update_list/2015-12-31.json
 		
 		if ($date) {
-			if (Util::is_date($date)) {
-				$opt = array('conditions' => array('modified >' => $date));
+			$dt = $this->__getFindSqlDate($date);
+			
+			if ($dt) {
+				$opt = array('conditions' => array('modified >' => $dt));
 				$meets = $this->Meet->find('list', $opt);
 			} else {
 				return $this->error('パラメタには日付を指定して下さい。', self::STATUS_CODE_BAD_REQUEST);
@@ -45,9 +47,27 @@ class ApiController extends AppController
 			$meets = $this->Meet->find('list', $opt);
 		}
 		
-		return $this->success(array('meets', $meets));
+		return $this->success(array('meets' => $meets));
 	}
 	
+	/**
+	 * SQL 条件に必要な日付オブジェクトをかえす
+	 * @param type $dateStr 日付の文字列表現
+	 * @return boolean 返還後文字列もしくは false をかえす
+	 */
+	private function __getFindSqlDate($dateStr)
+	{
+		if ($dateStr)
+		{
+			$dt = DateTime::createFromFormat('Y-m-d-H-i-s', $dateStr);
+			if ($dt)
+			{
+				return date_format($dt, 'Y-m-d H:i:s');
+			}
+		}
+		
+		return false;
+	}
 	 
 	//++++++++++++++++++++++++++++++++++++++++
 	// 以下、http://be-hase.com/php/478/ より拾いもの。
