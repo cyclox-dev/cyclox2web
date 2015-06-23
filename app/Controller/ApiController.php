9f4aa72d461d75e67996d086736c4a3ca9b7e42c
saki <saki@ginuuk.net>
2015-06-23 12:56:30 +0900

    ApiBaseController を作成。

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -1,6 +1,6 @@
 <?php
 
-App::uses('AppController', 'Controller');
+App::uses('ApiBaseController', 'Controller');
 
 App::uses('Util', 'Cyclox/Util');
 
@@ -14,16 +14,11 @@ App::uses('Util', 'Cyclox/Util');
  *
  * @author shun
  */
-class ApiController extends AppController
+class ApiController extends ApiBaseController
 {
 	public $uses = array('Meet');
 	
 	public $components = array('Session', 'RequestHandler');
-	
-	const STATUS_CODE_BAD_REQUEST = 400;
- 
-    // JSONやXMLにして返す値を格納するための配列です。
-    protected $result = array();
 	
 	/**
 	 * 更新情報についての ID, code などのリストを取得する。
@@ -68,67 +63,4 @@ class ApiController extends AppController
 		
 		return false;
 	}
-	 
-	//++++++++++++++++++++++++++++++++++++++++
-	// 以下、http://be-hase.com/php/478/ より拾いもの。
-	
-	public function beforeFilter() 
-	{
-        parent::beforeFilter();
-
-        // Ajaxでないアクセスは禁止。直アクセスを許すとXSSとか起きかねないので。
-        //if (!$this->request->is('ajax')) throw new BadRequestException('アクセスが許可されていません。');
-		// TODO: 必要ならばコメント外すこと。
-
-        //meta情報とかを返すといいですね。とりあえずいまアクセスしているurlとhttp methodでも含めておきましょう
-        $this->result['meta'] = array(
-            'url' => $this->request->here,
-            'method' => $this->request->method(),
-        );
-		$this->set('meta', $this->result['meta']);
-		
-        // nosniffつけるべし。じゃないとIEでXSS起きる可能性あり。
-        $this->response->header('X-Content-Type-Options', 'nosniff');
-    }
-	
-    // 成功系処理。$this->resultに値いれる
-    protected function success($response = array()) 
-	{
-        $this->result['response'] = $response;
- 
-        $this->set('meta', $this->result['meta']);
-        $this->set('response', $this->result['response']);
-    }
- 
-    // エラー系処理。$this->resultに値いれる
-    protected function error($message = '', $code = 0) 
-	{
-        $this->result['error']['message'] = $message;
-        $this->result['error']['code'] = $code;
- 
-        //ちゃんと400ステータスコードにするの大事。後述
-        
-		$this->set('error', $this->result['error']);
-		$this->response->statusCode(400);
-        //$this->set('_serialize', array('meta', 'error'));
-    }
- 
-    // バリデーションエラー系処理。$this->resultに値いれる
-    protected function validationError($modelName, $validationError = array())
-		{
-        $this->result['error']['message'] = 'Validation Error';
-        $this->result['error']['code'] = '422'; //エラーコードはプロジェクトごとに定義すべし
-        $this->result['error']['validation'][$modelName] = array();
-        foreach ($validationError as $key => $value) {
-            $this->result['error']['validation'][$modelName][$key] = $value[0];
-        }
- 
-        //ちゃんと400ステータスコードにするの大事。後述
-        $this->response->statusCode(400);
-        $this->set('meta', $this->result['meta']);
-        $this->set('error', $this->result['error']);
-        $this->set('_serialize', array('meta', 'error'));
-    }
-	
-	
 }
