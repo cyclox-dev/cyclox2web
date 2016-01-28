f3a15179b7e5a15bc3b198c0fb4c9f88d21d372f
saki <saki@ginuuk.net>
2016-01-28 17:28:13 +0900

    重複するカテゴリー所属データを deleted 設定する shell メソッドを実装。

--- app/Console/Command/OneTimeShell.php
+++ app/Console/Command/OneTimeShell.php
@@ -8,11 +8,11 @@
  * 1回だけの処理に使用する。コンソールから起動する。
  * 例）
  * > cd Cake/app
- * > Consle/cake one_time_shell TheMethodName arg,,,
+ * > Consle/cake one_time TheMethodName arg,,,
  */
 class OneTimeShell extends AppShell
 {
-	public $uses = array('EntryGroup', 'EntryRacer', 'Racer');
+	public $uses = array('EntryGroup', 'EntryRacer', 'Racer', 'CategoryRacer');
 	
     public function main() {
         $this->out('please input function name as 1st arg.');
@@ -21,7 +21,7 @@ class OneTimeShell extends AppShell
 	/**
 	 * EntryRacer.team_name から Racer.team を設定する。コンソールでの処理。起動方法は
 	 * > app ディレクトリ
-	 * > Console/cake one_time_shell setupTeamName
+	 * > Console/cake one_time setupTeamName
 	 */
 	public function setupTeamName()
 	{
@@ -91,5 +91,91 @@ class OneTimeShell extends AppShell
 		}
 		
 		$this->out('--- END ---');
+	}
+	
+	/**
+	 * 内容の重複する CategoryRacer に deleted ステータスを設定する
+	 * > app ディレクトリ
+	 * > Console/cake one_time setupDuplicatedCatRacerDeleted 0 100
+	 */
+	public function setupDuplicatedCatRacerDeleted()
+	{
+		if (!isset($this->args[0]) || !isset($this->args[1])) {
+			$this->out('2つの引数（整数、offset 位置, 処理件数）が必要です。');
+			return;
+		}
+		
+		//$this->out('offset:' . $this->args[0] . ' limit:' . $this->args[1]);
+		$offset = $this->args[0];
+		$limit = $this->args[1];
+		
+		$opt = array(
+			'limit' => 1,
+			'recursive' => -1,
+			'conditions' => array('deleted' => 0)
+		);
+		
+		//$crs = $this->CategoryRacer->find('all', $opt);
+		// find all, delete all では deleted されたものが重複する場合があるので1つ1つ for で回して処理する
+		
+		$i = $offset;
+		for (; $i < $offset + $limit; $i++)
+		{
+			$opt['offset'] = $i;
+			$catRacer = $this->CategoryRacer->find('first', $opt);
+			if (empty($catRacer)) break;
+			
+			$conditions = array(
+				'deleted' => 0,
+				'NOT' => array('id' => $catRacer['CategoryRacer']['id']),
+				'racer_code' => $catRacer['CategoryRacer']['racer_code'],
+				'category_code' => $catRacer['CategoryRacer']['category_code'],
+				// reason 系, result_id は同一性をチェックしない
+				'apply_date' => $catRacer['CategoryRacer']['apply_date'],
+			);
+			
+			if (empty($catRacer['CategoryRacer']['cancel_date'])) {
+				$conditions['cancel_date'] = null;
+			} else {
+				$conditions['OR'] = array(
+					array('cancel_date' => null),
+					array('cancel_date' => $catRacer['CategoryRacer']['cancel_date'])
+				);
+			}
+			
+			$opt2 = array(
+				'recursive' => -1,
+				'conditions' => $conditions
+			);
+			
+			$crs = $this->CategoryRacer->find('all', $opt2);
+			//$this->out('' . $i . ' crs size:' . count($crs));
+			
+			/*if (!empty($crs)) {
+				$this->log('conditions:', LOG_DEBUG);
+				$this->log($conditions, LOG_DEBUG);
+			}//*/
+			
+			foreach ($crs as $cr) {
+				$this->CategoryRacer->id = $cr['CategoryRacer']['id'];
+				
+				//$this->log($cr, LOG_DEBUG);
+				
+				if (!$this->CategoryRacer->delete()) {
+					$this->out('category racer [id:' . $cr['CategoryRacer']['id'] . '] の削除に失敗');
+					return;
+				} else {
+					$this->out('deleted id:' . $cr['CategoryRacer']['id'] . ' by ' . $catRacer['CategoryRacer']['id']);
+				}//*/
+			}
+			
+			if (!empty($crs)) {
+				$this->out('選手コード[' . $catRacer['CategoryRacer']['racer_code'] . ']の ' . count($crs) . '件のカテゴリー所属を削除しました。');
+			}
+		}
+		
+		if ($i - $offset < $limit) {
+			$this->out('指定した件数がありませんでした（処理終了の可能性）');
+		}
 	}
 }
