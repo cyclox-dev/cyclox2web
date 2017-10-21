c57329a31245e60216c5724e65136f04b1dc7b54
saki <saki@ginuuk.net>
2017-10-21 22:10:31 +0900

    setupTermOfSeriesPoint() を Model/MeetPointSeries に移動。

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -390,7 +390,7 @@ class ApiController extends ApiBaseController
 		
 		//++++++++++++++++++++++++++++++++++++++++
 		// meet point series の有効期間設定
-		$ret = $this->__setupTermOfSeriesPoint($meetCode, $ecatName);
+		$ret = $this->MeetPointSeries->setupTermOfSeriesPoint($meetCode, $ecatName);
 		// Transaction は使用せず
 		
 		if (!$ret) {
@@ -565,88 +565,6 @@ class ApiController extends ApiBaseController
 		}
 		
 		return $sfDist;
-	}
-	
-	private function __setupTermOfSeriesPoint($meetCode, $ecatName)
-	{
-		if (empty($meetCode) || empty($ecatName)) {
-			return false;
-		}
-		
-		$opt = array(
-			'conditions' => array(
-				'meet_code' => $meetCode,
-			)
-		);
-		$mpss = $this->MeetPointSeries->find('all', $opt);
-		
-		$tmpMpss = array();
-		foreach ($mpss as $mps) {
-			// mps.ecat_name にワイルドカードが含まれるのでプログラム側で走査
-			$mpsEcat = $mps['MeetPointSeries']['entry_category_name'];
-			if ($mpsEcat === $ecatName) {
-				$tmpMpss[] = $mps;
-			} else if ($this->__endsWith($mpsEcat, '*')) {
-				$nameBody = substr($mpsEcat, 0, -1);
-				if ($this->__startsWith($ecatName, $nameBody)) {
-					$tmpMpss[] = $mps;
-				}
-			}
-		}
-		$mpss = $tmpMpss;
-		
-		if (empty($mpss)) {
-			return true;
-		}
-		
-		foreach ($mpss as $mps) {
-			$termRuleVal = $mps['PointSeries']['point_term_rule'];
-			$rule = PointSeriesTermOfValidityRule::ruleAt($termRuleVal);
-			if (is_null($rule)) {
-				$this->log('期間設定ルールを設定できません。', LOG_DEBUG);
-				return false;
-			}
-			
-			if (!empty($rule)) {
-				$term = $rule->calc($mps['Meet']['at_date']);
-				
-				$newMps = array();
-				$newMps['MeetPointSeries'] = array();
-				
-				if (!empty($term['begin'])) {
-					$newMps['MeetPointSeries']['point_term_begin'] = $term['begin'];
-				} else {
-					$newMps['MeetPointSeries']['point_term_begin'] = null;
-				}
-				if (!empty($term['end'])) {
-					$newMps['MeetPointSeries']['point_term_end'] = $term['end'];
-				} else {
-					$newMps['MeetPointSeries']['point_term_end'] = null;
-				}
-				
-				//$this->log($mps, LOG_DEBUG);
-				
-				if (!empty($term['begin']) || !empty($term['end'])) {
-					$this->MeetPointSeries->id = $mps['MeetPointSeries']['id'];
-					$ret = $this->MeetPointSeries->save($newMps);
-					
-					if (!$ret) {
-						$this->log('MeetPointSeries の保存に失敗しました。', LOG_ERR);
-						return false;
-					}
-				}
-			}
-		}
-		
-		return true;
-	}
-	
-	private function __startsWith($haystack, $needle) {
-		return substr($haystack, 0, strlen($needle)) === $needle;
-	}
-	
-	private function __endsWith($haystack, $needle) {
-		return substr($haystack, - strlen($needle)) === $needle;
 	}
 	
 	/**
