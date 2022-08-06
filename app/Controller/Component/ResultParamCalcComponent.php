23b4f3944a5b84a58fe3e5cb0fd79b90cf8c04be
kentaro <kentaro@kentaronoMacBook-Air.local>
2022-08-06 14:52:05 +0900

    is_jcxフラグ取得処理を追加

--- app/Controller/Component/ResultParamCalcComponent.php
+++ app/Controller/Component/ResultParamCalcComponent.php
@@ -149,28 +149,43 @@ class ResultParamCalcComponent extends Component
 	 */
 	private function __doReCalcResults($results, $ecat)
 	{
-		if (empty($results) || empty($ecat)) {
+        // Meetからis_jcxフラグを取得する
+		$this->EntryCategory = new EntryCategory();
+		$this->EntryCategory->Behaviors->load('Containable');
+		$options = array(
+			'conditions' => array('EntryCategory.' . $this->EntryCategory->primaryKey => $ecat['id']),
+			'contain' => array(
+				'EntryGroup' => array(
+					'Meet'
+				)
+			)
+		);
+
+        $ret = $this->EntryCategory->find('first', $options);
+        $isJcx = $ret['EntryGroup']['Meet']['is_jcx'];
+
+        if (empty($results) || empty($ecat)) {
 			return Constant::RET_ERROR;
 		}
-		
+
 		$this->__setupParams($results);
 		$this->__setupMeetParams($ecat);
 		$this->__setupRankUpRules();
-		
+
 		// ajocc point の適用
 		foreach ($results as $result) {
 			$r = $result['RacerResult'];
-			
+
 			// 1920からは ajocc_pt = null 設定あり。null な箇所のポイントは平均に参入しない。出走レース数にも含めない。
 			$ajoccPt = ($this->_isSeasonAfter1819()) ? null : 0;
-			
+
 			$isOpenRacer = ($result['EntryRacer']['entry_status'] == RacerEntryStatus::$OPEN->val());
-			
-			$point = empty($r['rank']) ? 0 : $this->calcAjoccPt($r['rank'], $this->__started, $this->__atDate);
-			
+
+			$point = empty($r['rank']) ? 0 : $this->calcAjoccPt($r['rank'], $this->__started, $this->__atDate, $isJcx);
+
 			if ($ecat['applies_ajocc_pt']) {
-				
-				
+
+
 				if (!$isOpenRacer
 						&& !($this->_isSeasonAfter1819() && $r['status'] == RacerResultStatus::$DNS->val()))
 						// DNS はポイント null @kgym@20190814
@@ -220,9 +235,10 @@ class ResultParamCalcComponent extends Component
 	 * @param type $rank リザルト順位
 	 * @param type $startedCount 出走人数
 	 * @param dateString $meetDate 大会日付
+	 * @param Boolean $isJcx JCX大会か否かのフラグ
 	 * @return int ポイント。エラーの場合 -1 をかえす。リザルトステータスは見ず、ポイントなしはゼロをかえす。
 	 */
-	public function calcAjoccPt($rank, $startedCount, $meetDate)
+	public function calcAjoccPt($rank, $startedCount, $meetDate, $isJcx)
 	{
 		if ($startedCount <= 0) {
 			return -1;
